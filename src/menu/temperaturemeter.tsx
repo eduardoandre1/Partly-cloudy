c184d1a74b1d5bd51245c1ffa9fb0672e646d803
Eduardo André <eduardo_andre@id.uff.br>
2023-12-03 19:06:17 -0300

    fix menu style

--- src/menu/temperaturemeter.tsx
+++ src/menu/temperaturemeter.tsx
@@ -1,5 +1,6 @@
 import { useContext } from 'react';
 import styled from 'styled-components';
+import image from '../resources/01d@2x 1.png'
 
 import ProviderContect from '../provider';
 
@@ -9,14 +10,15 @@ function TemperatureMeter() {
 
   return (
     <TemperatureBarStyle>
-      <>
-        <h1>{global.apiRespost ? global.apiRespost.main.temp + 'ºC' : '--'}</h1>
+      <Espaço>
+        <img src={image} />
+        <h1>{global.apiRespost ? parseInt(global.apiRespost.main.temp) + 'ºC' : '--'}</h1>
         <br />
-      </>
+      </Espaço>
       <h2>
         {global.apiRespost ? global.apiRespost.weather[0].description : ' no data '}
       </h2>
-      <div></div>
+      <Line />
     </TemperatureBarStyle>
   );
 }
@@ -24,30 +26,28 @@ export default TemperatureMeter;
 
 const TemperatureBarStyle = styled.div`
   width: 426px;
-  height: 412px;
+  height: 490px;
   display: flex;
   flex-direction: column;
-  flex-wrap: nowrap;
-  justify-content: flex-start;
-  align-items: center;
-  div {
-    background: #ededed;
-    width: 395px;
-    height: 5px;
+  flex-wrap: wrap;
+  img {
+    height: 150px;
+    width: 150px;
   }
   h1 {
     width: 100%;
     height: 90px;
-    flex-shrink: 0;
+    width: 130px;
     color: #ec6e4c;
     font-family: Poppins;
-    font-size: 150px;
+    font-size: 120px;
     font-style: normal;
     font-weight: 300;
     line-height: 48px; /* 32% */
     text-align: center;
   }
   h2 {
+    margin-top: 95px;
     height: 42px;
     color: #222;
     font-family: Poppins;
@@ -56,5 +56,19 @@ const TemperatureBarStyle = styled.div`
     font-weight: 400;
     line-height: 48px; /* 150% */
     text-align: center;
+    width: 100%;
   }
 `;
+const Espaço = styled.div`
+display: flex;
+    flex-direction: row;
+    justify-content: flex-start;
+    align-items: flex-start;
+    height: 90px;
+    width: 490px;
+`
+const Line = styled.div`
+ background: #ededed;
+    width: 395px;
+    height: 5px;
+`
